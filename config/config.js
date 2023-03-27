ead9396cdfceddab633a5bfec356c57ee1ef8a67
RayHakku <104810422+RayHakku@users.noreply.github.com>
2023-03-27 10:26:28 -0300

    refactor: all models deleted

---
+++ config/config.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+module.exports = {
+    development: {
+        url: 'postgres://benggpud:lAPFH_ioTojm-PRax0yoitPFjYGd7Wnn@babar.db.elephantsql.com/benggpud',
+        dialect: 'postgres',
+    },
+    test: {
+        url: process.env.TEST_DATABASE_URL,
+        dialect: 'postgres',
+    },
+    production: {
+        url: process.env.DATABASE_URL,
+        dialect: 'postgres',
+    },
+}
--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
@@ -42,11 +42,5 @@ db.Sequelize = Sequelize;
 
 db.usuarios = require("./usuario")(sequelize, sequelize);
 db.contas = require("./conta")(sequelize, sequelize);
-
-db.usuarios.hasMany(db.contas, { as: "contas" });
-db.contas.belongsTo(db.usuarios, {
-  foreignKey: "userId",
-  as: "usuario"
-});
 
 module.exports = db;
